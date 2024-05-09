2cbcf4e67d9dcac81cf6d42b2e1bf98ace6080b3
Mory DIA <d363519@heb.com>
2024-05-09 17:17:28 -0500

    Lazy Loading modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ElementsModule } from './elements/elements.module';
-import { COllectionsModule } from './collections/collections.module';
 import { ModsModule } from './mods/mods.module';
 import { ViewsModule } from './views/views.module';
 import { HomeComponent } from './home/home.component';
@@ -18,8 +16,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    ElementsModule,
-    COllectionsModule,
     ViewsModule,
     ModsModule,
     AppRoutingModule
