b45684b5f92a6dd6973f397d0fb329ae69b3d8b5
Mory DIA <dia.mory@heb.com>
2024-06-03 14:11:10 -0500

    Template Froms Wraps Up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bootcamp-rooting';
+
+  email = '';
+
+  onSubmit(){
+    console.log(this.email)
+  }
 }
